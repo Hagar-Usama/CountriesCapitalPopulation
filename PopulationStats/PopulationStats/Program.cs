ce0e923a3460b96c997e633e51a1d3158bb57b51
hagar usama <eng-hagar.usama1621@alexu.edu.eg>
2022-06-06 14:59:30 +0200

    tested input to memDB, commented unused code in program

--- PopulationStats/PopulationStats/Program.cs
+++ PopulationStats/PopulationStats/Program.cs
@@ -2,8 +2,6 @@
 //using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using PopulationStats.Models;
-using Microsoft.Net.Http.Headers;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,20 +13,9 @@ builder.Services.AddDbContext<PopuContext>(opt =>
 builder.Services.AddEndpointsApiExplorer();
 
 // reading external apis
-builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
-builder.Services.AddHttpClient("GitHub", httpClient =>
-{
-    httpClient.BaseAddress = new Uri("https://api.github.com/");
-
-    // using Microsoft.Net.Http.Headers;
-    // The GitHub API requires two headers.
-    httpClient.DefaultRequestHeaders.Add(
-        HeaderNames.Accept, "application/vnd.github.v3+json");
-    httpClient.DefaultRequestHeaders.Add(
-        HeaderNames.UserAgent, "HttpRequestsSample");
-});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
